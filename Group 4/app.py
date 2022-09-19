b9541099aee09e44cc2d1e460ee63f90388263a7
satman0491 <satmanret@gmail.com>
2022-09-19 02:41:37 +0100

    UI making

--- Group 4/app.py
+++ Group 4/app.py
@@ -1,9 +1,34 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir,'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFIATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
